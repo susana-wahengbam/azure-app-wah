8fc215ecf6747d531e171e04fa601bbeeaa73bba
Susana Wahengbam <kkluwang@gmail.com>
2024-06-28 05:31:07 -0400

    Add project files.

---
+++ azure-app-wah-vs/Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace azure_app_wah_vs.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        private readonly IConfiguration _configuration;
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public void OnGet()
+        {
+            ViewData["Greetings"] = _configuration["Greetings"];
+        }
+    }
+}
